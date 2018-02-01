53c436fdc9a0ed28c780ae81d2a0e54f9983b219
Trent Gardner <trent.gardner@gmail.com>
2018-02-01 21:02:03 +1030

    Updated javascript definition

--- www/androidwear.js
+++ www/androidwear.js
@@ -1,19 +1,25 @@
+/* global cordova */
 
 var exec = cordova.require('cordova/exec'),
-	service = "androidwear";
+    service = "androidwear";
 
-exports.onConnect = function(win, fail) {
-	exec(win, fail, service, "onConnect", []);
+var AndroidWear = function() {};
+
+AndroidWear.prototype.onConnect = function(win, fail) {
+    exec(win, fail, service, "onConnect", []);
 };
 
-exports.onDataReceived = function(handle, win, fail) {
-	exec(win, fail, service, "onDataReceived", [handle]);
+AndroidWear.prototype.onDataReceived = function(handle, win, fail) {
+    exec(win, fail, service, "onDataReceived", [handle]);
 };
 
-exports.onError = function(handle, win, fail) {
-	exec(win, fail, service, "onError", [handle]);
+AndroidWear.prototype.onError = function(handle, win, fail) {
+    exec(win, fail, service, "onError", [handle]);
 };
 
-exports.sendData = function(handle, data, win, fail) {
-	exec(win, fail, service, "sendData", [handle, data]);
+AndroidWear.prototype.sendData = function(handle, data, win, fail) {
+    exec(win, fail, service, "sendData", [handle, data]);
 };
+
+var androidwear = new AndroidWear();
+module.exports = androidwear;
